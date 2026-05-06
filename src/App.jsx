62c6b0a264ca5ce97d42eb7c28a1ebe81c93728e
randy <shreevesrandy@gmail.com>
2026-05-06 10:58:42 -0400

    Added component to retrieve user's tasks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = async() => {
+  const handleLogin = async () => {
     try {
       const response = await fetch("https://springboot-todo-api-z09g.onrender.com/auth/login", {
         method: "POST",
@@ -20,9 +20,28 @@ function App() {
 
       const data = await response.json();
       console.log("TOKEN:", data.token);
+      localStorage.setItem("token", data.token);
 
     } catch (error) {
       console.error("Login failed", error);
+    }
+  };
+
+  const fetchTasks = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await fetch("https://springboot-todo-api-z09g.onrender.com/tasks", {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+      console.log("TASKS:", data);
+    } catch (error) {
+      console.error("Fetch tasks failed", error);
     }
   };
 
@@ -51,7 +70,9 @@ function App() {
       <br />
 
       <button onClick={handleLogin}>Login</button>
+      <button onClick={fetchTasks}>Get Tasks</button>
     </div>
+  
   );
 }
 
