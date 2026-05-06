c849c9cd53c12e790aed69604dc862736457d658
randy <shreevesrandy@gmail.com>
2026-05-05 20:25:30 -0400

    Initital React frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async() => {
+    try {
+      const response = await fetch("https://springboot-todo-api-z09g.onrender.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      });
+
+      const data = await response.json();
+      console.log("TOKEN:", data.token);
+
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+
+      <h2>Login</h2>
+
+      <input
+      type="text"
+      placeholder="Username"
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <br />
+
+      <input
+      type="password"
+      placeholder="Password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default App
